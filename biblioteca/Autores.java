bede771bef82545bb60e874e90d5d38b206a0202
GabrielFNP <102600004+GabrielFNP@users.noreply.github.com>
2022-05-30 20:37:14 -0300

    Biblioteca

---
+++ biblioteca/Autores.java
@@ -0,0 +1,27 @@
+package poo2.biblioteca;
+
+public class Autores {
+    private String nome;
+    private String titulacao;
+
+    public Autores(String nome, String titulacao) {
+        this.nome = nome;
+        this.titulacao = titulacao;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setTitulacao(String titulacao) {
+        this.titulacao = titulacao;
+    }
+    public String getTitulacao() {
+        return titulacao;
+    }
+
+}
